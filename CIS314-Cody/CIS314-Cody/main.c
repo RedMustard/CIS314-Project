6759ebf32e5577d7fafcc6f11f183cc94a1b5b6f
Cody Ebert <cebert@uoregon.edu>
2015-11-08 15:51:51 -0800

    Cody
    lol

--- CIS314-Cody/CIS314-Cody/main.c
+++ CIS314-Cody/CIS314-Cody/main.c
@@ -7,9 +7,29 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(int argc, const char * argv[])
+{
+  
+    
+    return 0;
+}
+int ALU(int *arg1, int *arg2, char* command)
+{
+    int result = 0;
+    if(strcmp(command, "add") == 0)
+    {
+        result = *arg1 + *arg2;
+        return result;
+    }
+    else if(strcmp(command, "subtract") == 0)
+    {
+        result = *arg1 - *arg2;
+        return result;
+    }
     return 0;
 }
--- cody.c
+++
@@ -1,9 +0,0 @@
-//
-//  cody.c
-//  
-//
-//  Created by Cody C Ebert on 11/8/15.
-//
-//
-
-#include "cody.h"

bc589ca38b5d09c297ddd7c2daf041c9c4715e85
Alex G <alexandrgeoffrey@Alexs-MacBook-Pro-94.local>
2015-11-08 15:51:30 -0800

    Fetch/decode functions
    C Code for the fetch/decode functions

---
+++ Fetch_Decode/Fetch_Decode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Fetch_Decode
+//
+//  Created by Alex Geoffrey on 11/8/15.
+//  Copyright © 2015 MooseMen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

aa57874bb9517e846e0d7005d3e7f915af7fdd26
Travis Barnes <ttb@uoregon.edu>
2015-11-08 18:15:17 -0800

    New Project Init Commit
    New local project created. Initial commit.

---
+++ CIS314-Travis/CIS314-Travis/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CIS314-Travis
+//
+//  Created by Travis Barnes on 11/8/15.
+//  Copyright (c) 2015 Travis Barnes. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
